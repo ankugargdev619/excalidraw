4b94d6e3f72f10968a5fdf25052b8b80b6b800cd
ankugargdev619 <ankugargdev619@gmail.com>
2025-01-22 21:01:25 +0530

    Fix auth logic

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,43 +1,135 @@
 import express, { json } from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import {JWT_SECRET} from "@repo/environment/config";
+import {SALT} from "@repo/environment/config";
 import { checkAuth } from "./middlewares/checkAuth";
-import {CreateUserSchema} from "@repo/common/types";
+import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types";
+import {prismaClient} from "@repo/db/client";
 const app = express();
 app.use(express.json());
 
 
-app.post("/signup",(req,res)=>{
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
-        res.json({
-            message : "Incorrect input"
+app.post("/signup",async (req,res)=>{
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
+        const issue = parsedData.error?.issues[0];
+        res.status(411).json({
+            message : `${issue?.path[0]} ${issue?.message}` || "Incorrect input"
         })
         return
     }
+
+    
+
+    try{
+        const hashPass = await bcrypt.hash(parsedData.data.password,SALT);
+        const user = await prismaClient.user.create({
+            data : {
+                name : parsedData.data?.name,
+                // TODO hash the password
+                email : parsedData.data.username,
+                password : hashPass
+            }
+        })
+    } catch(e){
+        console.log(e);
+        res.status(411).json({
+            message : "User is already registered"
+        });
+        return
+    }
+
     res.json({
         "message" : "User is created!"
     })
 });
 
-app.post("/signin",(req,res)=>{
-    const userId = req.body.userId;
-    const password = req.body.password;
-    const token = jwt.sign({
-                        userId
-                    },JWT_SECRET);
+app.post("/signin",async (req,res)=>{
+    const parsedData = SigninSchema.safeParse(req.body);
 
-    res.json({
-        token
-    })
+    if(!parsedData.success){
+        const issue = parsedData.error?.issues[0];
+        res.status(411).json({
+            message : `${issue?.path[0]} ${issue?.message}` || "Incorrect input"
+        })
+        return
+    }
+
+    try{
+        const user = await prismaClient.user.findFirst({
+            where : {
+                email : parsedData.data.username
+            }
+        })
+
+        if(!user){
+            res.json({
+                message : "User not found"
+            });
+            return
+        }
+
+        const isValidPass = await bcrypt.compare(parsedData.data.password,user?.password)
+        if(!isValidPass){
+            res.status(411).json({
+                message : "Your password is incorrect"
+            });
+            return;
+        }
+
+        const token = jwt.sign({
+            username : user?.email
+        },JWT_SECRET)
+
+        res.json({
+            token : token
+        })
+        return
+
+    } catch(e){
+        console.log(e);
+        res.status(411).json({
+            message : "User does not exist"
+        });
+        return
+    }
+
 })
 
 
-app.post("/room",checkAuth,(req,res)=>{
-    const roomId = "1234"
-    res.json({
-        roomId
-    })
+app.post("/room",checkAuth,async (req,res)=>{
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if(!parsedData.success){
+        const issue = parsedData.error?.issues[0];
+        res.status(411).json({
+            message : `${issue?.path[0]} ${issue?.message}` || "Incorrect input"
+        })
+        return;
+    }
+
+    const userId = req.userId;
+    console.log(userId);
+
+    try{        
+        const room = await prismaClient.room.create({
+            data :{
+                slug : parsedData?.data.name,
+                adminId : userId
+            }
+        })
+        console.log(room);
+        res.json({
+            room
+        })
+        return
+    } catch(e){
+        console.log(e);
+        res.status(411).json({
+            message : "Error creating room!"
+        })
+        return
+    }
 })
 
 app.listen(3001);
--- apps/http-backend/src/middlewares/checkAuth.ts
+++ apps/http-backend/src/middlewares/checkAuth.ts
@@ -3,12 +3,21 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import {JWT_SECRET} from "@repo/environment/config";
 
 export function checkAuth(req : Request ,res : Response,next : NextFunction){
-    const token = req.headers.authorization ?? "";
-    const decoded = jwt.verify(token,JWT_SECRET) as JwtPayload;
-
-    if(decoded){
-        req.userId = decoded.userId;
-        next();
+    try{
+        const token = req.headers.authorization?.split(" ")[1] ?? "";
+        const decoded = jwt.verify(token,JWT_SECRET) as JwtPayload;
+        console.log(decoded);
+        if(decoded){
+            req.userId = decoded.username;
+            console.log("User is authenticated");
+            next();
+        } else {
+            res.status(403).json({message : "You are not authorized!"});
+            return;
+        }
+    } catch(e){
+        res.status(403).json({message : "Error while authenticated!"});
+        return;
     }
-    res.status(403).json("You are not authorized!");
+    
 }
